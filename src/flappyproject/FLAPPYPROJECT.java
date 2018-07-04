9ad2f82e003387baf2edf6937e9825199a11f2c1
marielosagui <37224129+marielosagui@users.noreply.github.com>
2018-07-04 16:56:40 -0600

    Proyecto

---
+++ src/flappyproject/FLAPPYPROJECT.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flappyproject;
+
+import Interfaz.Juego;
+
+/**
+ *
+ * @author karla
+ */
+public class FLAPPYPROJECT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+            new Juego().setVisible(true);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+    
+}
